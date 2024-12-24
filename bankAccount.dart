cd99e45edb670b8c69609977d44be9e62ee173e6
Sara Waleed <116273800+sarawaleed13@users.noreply.github.com>
2024-12-24 21:44:34 +0200

    Update bankAccount.dart

--- bankAccount.dart
+++ bankAccount.dart
@@ -17,7 +17,7 @@ class BankAccount {
       print(' Balance is not suffectent .');
     } else {
       balance -= amount;//deduct amount;
-      
+        print('Withdrawal of \$${amount.toStringAsFixed(2)} successful.');
     }
   }
 
@@ -27,6 +27,7 @@ class BankAccount {
       print('Not positive.');
     } else {
       balance += amount;
+       print('Deposit of \$${amount.toStringAsFixed(2)} successful.');
     }
   }
 
@@ -40,8 +41,20 @@ void main(){
 
   var acc1 = BankAccount('85637', 100);
   print(' ${acc1.account} ${acc1.balance}');
-
+  // Perform deposit and withdrawal operations for Account 1
+  acc1.deposit(100); // Valid deposit
+  acc1.withdraw(100); // Valid withdrawal
+  acc1.withdraw(1000.0); // Insufficient balance
+  acc1.withdraw(-100.0); // Invalid withdrawal amount
+  acc1.deposit(-100.0); // Invalid deposit amount
+  acc1.displayAccountInfo();
+  print('//*********************************************** */');
   var acc2 = BankAccount.forBalance('85634');
   print(' ${acc2.account} ${acc2.balance}');
-
+  // Perform deposit and withdrawal operations for Account 1
+  acc2.deposit(100); // Valid deposit
+  acc2.withdraw(100); // Valid withdrawal
+  acc2.withdraw(1000.0); // Insufficient balance
+  acc2.withdraw(-100.0); // Invalid withdrawal amount
+  acc2.deposit(-100.0); // Invalid deposit amount
 }
